c6aaa051672f6d6d101a5710963f87a2c5daa2e3
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2017-11-28 17:43:10 +0600

    context processor bug fixed

--- packages/main_app/homepage_context_processors.py
+++ packages/main_app/homepage_context_processors.py
@@ -2,18 +2,17 @@ from .models import Category
 
 
 def header(request):
-    
     categories = Category.objects.all()
     
     dummy_pages = [
-        "পুরস্কার জিতুন",#"Get a prize",
-        "জনপ্রিয় মিম",#"Top MeMes",
-        "জনপ্রিয় বিভাগ", #"Top Categories",
-        "জনপ্রিয় পোস্টদাতা",#"Best meme-guy",
-        "লগইন",#"login",
-        "সাইন আপ",#"sign-up",
-        "এপ ব্যবহার করুন",#"use app",
-        "",#"contact",
+        "পুরস্কার জিতুন",  # "Get a prize",
+        "জনপ্রিয় মিম",  # "Top MeMes",
+        "জনপ্রিয় বিভাগ",  # "Top Categories",
+        "জনপ্রিয় পোস্টদাতা",  # "Best meme-guy",
+        "লগইন",  # "login",
+        "সাইন আপ",  # "sign-up",
+        "এপ ব্যবহার করুন",  # "use app",
+        # " যোগাযোগ করুন",#"contact",
     ]
     
     social_site_font = [
@@ -24,9 +23,9 @@ def header(request):
     ]
     
     return {
-        "categories" : categories,  # showing categories in header at base.html
-        "limit" : 5,    # limit for showing category in header menu (other categories will be shown in a dropdown menu)
+        "hcp_categories": categories,  # showing categories in header at base.html
+        "hcp_limit": 5,  # limit for showing category in header menu (other categories will be shown in a dropdown menu)
         
-        "dummy_pages" : dummy_pages,    # dummy pages for header menu
-        "socials" : social_site_font,   # social site's font awesome icon
+        "hcp_dummy_pages": dummy_pages,  # dummy pages for header menu
+        "hcp_socials": social_site_font,  # social site's font awesome icon
     }
